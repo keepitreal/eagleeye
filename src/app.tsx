740b02c76a8e621d6ff9abbd1d45a25af96a8214
Darion Welch <darionwelch@invisionapp.com>
2020-09-05 21:38:15 -0500

    initial commit

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div>Welcome to eagle eye</div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  devtool: 'eval-source-map',
+  entry: './src/app.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
